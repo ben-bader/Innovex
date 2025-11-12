9167971b32fb10c12d3dce22985b8a52b065771c
BADER_IN_4K <benbader1231@gmail.com>
2025-11-12 23:05:01 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators:false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**", // allow all paths under this domain
+      },
+    ],
+  },
+
 };
 
 export default nextConfig;
